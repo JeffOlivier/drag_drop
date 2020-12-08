8f1e01e1f74d43a2c9e4bbff5a908f46b02a7e19
Jeff Olivier <jeff@jeffolivier.com>
2020-12-08 10:34:38 -0500

    Removes the file server.js

--- server.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var app = express();
-
-app.use(express.static(__dirname + '/public')); //__dir and not _dir
-
-var port = 8000; // you can use any port
-app.listen(port);
-console.log('Running node.js server on port ' + port);
